d4e0f49958613e285d7a8af4a134de631a09f793
Vladimir_Makarov <stan13.09@mail.ru>
2022-08-01 17:15:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,10 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function parseToMorse(str) {
+    let strReplace = str.replace(/00/g,'');
+    strReplace = strReplace.replace(/10/g,'.');
+    strReplace = strReplace.replace(/11/g,'-');
+    return strReplace;
+}
+
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let delStr = String(expr);
+    let arrayOfString = [];
+
+    let indexChar = 0;
+    while (delStr.length>=10) {
+        let subStr = delStr.slice(indexChar, indexChar + 10);
+        if (subStr === '**********') {
+            subStr = ' ';
+        }
+        arrayOfString.push(subStr);
+        delStr = delStr.slice(10, delStr.length);        
+    }
+
+    for (let index = 0; index < arrayOfString.length; index++) {
+        if (arrayOfString[index] != ' ') {
+            arrayOfString[index] = parseToMorse(arrayOfString[index]);  
+        }      
+    }
+    
+    for (let index = 0; index < arrayOfString.length; index++) {
+        if (arrayOfString[index] != ' ') {
+            result += MORSE_TABLE[arrayOfString[index]];
+        }
+        else {
+            result += ' ';
+        }
+        
+    }
+    
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+//console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
